8385f44e3ae602a768576ff8236da03a7cf0eb4f
cs252sp20ta1 <cs252sp20ta1@cs252sp20ta1-13630.pods.cs252sp20ta1.svc.cluster.local>
2020-05-25 16:11:07 -0700

    use original model.py

--- hw3_jau_youyi/Segmentation/model.py
+++ hw3_jau_youyi/Segmentation/model.py
@@ -72,11 +72,11 @@ class encoder(nn.Module ):
 
 
 class decoder(nn.Module ):
-    def __init__(self, x5=512, x4=256, x3=128):
+    def __init__(self ):
         super(decoder, self).__init__()
-        self.conv1 = nn.Conv2d(x5+x4+x3, x5, 3, 1, 1, bias=False )
-        self.bn1 = nn.BatchNorm2d(x5 )
-        self.conv1_1 = nn.Conv2d(x5, 21, 3, 1, 1, bias=False )
+        self.conv1 = nn.Conv2d(512+256+128, 512, 3, 1, 1, bias=False )
+        self.bn1 = nn.BatchNorm2d(512 )
+        self.conv1_1 = nn.Conv2d(512, 21, 3, 1, 1, bias=False )
         self.bn1_1 = nn.BatchNorm2d(21)
         self.conv2 = nn.Conv2d(64+21, 21, 3, 1, 1, bias=False )
         self.bn2 = nn.BatchNorm2d(21 )
@@ -118,92 +118,56 @@ class encoderDilation(nn.Module ):
         super(encoderDilation, self ).__init__()
 
         ## IMPLEMENT YOUR CODE HERE
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64 )
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        self.b1_1 = ResBlock(64, 64, 1)
-        self.b1_2 = ResBlock(64, 64, 1)
-
-        self.b2_1 = ResBlock(64, 128, 2)
-        self.b2_2 = ResBlock(128, 128, 1)
-
-        self.b3_1 = ResBlock(128, 256, stride=1, dilation=2)
-        self.b3_2 = ResBlock(256, 256, stride=1, dilation=2)
-
-        self.b4_1 = ResBlock(256, 512, stride=1, dilation=4)
-        self.b4_2 = ResBlock(512, 512, stride=1, dilation=4)
 
     def forward(self, im ):
 
         ## IMPLEMENT YOUR CODE HERE
-        x1 = F.relu(self.bn1(self.conv1(im) ), inplace=True)
-        x2 = self.b1_2(self.b1_1(self.maxpool(x1 ) ) )
-        x3 = self.b2_2(self.b2_1(x2 ) )
-        x4 = self.b3_2(self.b3_1(x3 ) )
-        x5 = self.b4_2(self.b4_1(x4 ) )
+
         return x1, x2, x3, x4, x5
 
 
-# class decoderDilation(nn.Module ):
-class decoderDilation(decoder):
+class decoderDilation(nn.Module ):
     def __init__(self, isSpp = False ):
         super(decoderDilation, self).__init__()
 
         ## IMPLEMENT YOUR CODE HERE
 
-    # def forward(self, im, x1, x2, x3, x4, x5):
+    def forward(self, im, x1, x2, x3, x4, x5):
 
-    #     ## IMPLEMENT YOUR CODE HERE
+        ## IMPLEMENT YOUR CODE HERE
 
 
-    #     return pred
+        return pred
 
-# ref: https://github.com/Lextal/pspnet-pytorch/blob/master/pspnet.py
-class PSPModule(nn.Module):
-    def __init__(self, features, out_features=1024, sizes=(1, 2, 3, 6)):
-        # super().__init__()
-        super(PSPModule, self ).__init__()
-        self.stages = []
-        self.stages = nn.ModuleList([self._make_stage(features, size) for size in sizes])
-        self.bottleneck = nn.Conv2d(features * (len(sizes) + 1), out_features, kernel_size=1)
-        self.relu = nn.ReLU()
 
-    def _make_stage(self, features, size):
-        prior = nn.AdaptiveAvgPool2d(output_size=(size, size))
-        conv = nn.Conv2d(features, features, kernel_size=1, bias=False)
-        return nn.Sequential(prior, conv)
 
-    def forward(self, feats):
-        h, w = feats.size(2), feats.size(3)
-        priors = [F.interpolate(input=stage(feats), size=(h, w), mode='bilinear') for stage in self.stages] + [feats]
-        bottle = self.bottleneck(torch.cat(priors, 1))
-        return self.relu(bottle)
-
-class encoderSPP(encoderDilation):
+class encoderSPP(nn.Module ):
     def __init__(self ):
-        super(encoderSPP, self ).__init__()
-        self.psp = PSPModule(features = 512, out_features=1024, sizes=(1, 2, 3, 6))
+        super(encoderDilation, self ).__init__()
+
         ## IMPLEMENT YOUR CODE HERE
 
     def forward(self, im ):
-        x1, x2, x3, x4, x5 = super().forward(im)
-        x5 = self.psp(x5)
+
         ## IMPLEMENT YOUR CODE HERE
 
         return x1, x2, x3, x4, x5
 
 
-class decoderSPP(decoder):
+class decoderSPP(nn.Module ):
     def __init__(self, isSpp = False ):
-        super(decoderSPP, self).__init__(x5=1024)
+        super(decoderDilation, self).__init__()
+
         ## IMPLEMENT YOUR CODE HERE
 
-    # def forward(self, im, x1, x2, x3, x4, x5):
-        ## IMPLEMENT YOUR CODE HERE
-        # return pred
+    def forward(self, im, x1, x2, x3, x4, x5):
 
-# from model import loadPretrainedWeight
+        ## IMPLEMENT YOUR CODE HERE
+
+
+        return pred
+
+
 def loadPretrainedWeight(network, isOutput = False ):
     paramList = []
     resnet18 = resnet.resnet18(pretrained=True )
@@ -213,34 +177,12 @@ def loadPretrainedWeight(network, isOutput = False ):
     cnt = 0
     for param in network.parameters():
         if paramList[cnt ].size() == param.size():
-            # param.data.copy_(param.data )
-            param.data.copy_(paramList[cnt ].data )
+            param.data.copy_(paramList[cnt].data )
+            #param.data.copy_(param.data )
             if isOutput:
                 print(param.size() )
         else:
             print(param.shape, paramList[cnt].shape )
             break
         cnt += 1
- 
-def test_PSP():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = encoderSPP()
-    model = model.to(device)
-    loadPretrainedWeight(model, isOutput=True)
 
-    # check keras-like model summary using torchsummary
-    from torchsummary import summary
-    summary(model, input_size=(3, 512, 512))    
-
-def main():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = encoder()
-    model = model.to(device)
-    loadPretrainedWeight(model, isOutput=True)
-
-    # check keras-like model summary using torchsummary
-    from torchsummary import summary
-    summary(model, input_size=(3, 512, 512))
-
-if __name__ == '__main__':
-    main()
